2dea062dba4d249b977718412d2aaf15eda375fd
Juniper Alcorn <jalcorn@ool-ad02028b.dyn.optonline.net>
2019-03-26 11:32:53 -0400

    Add Measure Images

--- src/components/Measure.js
+++ src/components/Measure.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react'
 import {Route, Link} from 'react-router-dom'
+import '../styles/Measure.css';
 
 class Measure extends Component{
+
     render(){
         return(
             <div className='measure'>
                 <h1>Measure</h1>
-                <p>image goes here</p>
+                
+                <p>2 inches</p>
+                <div className='inch-measurement'>
+                    <div className='length'>|</div>
+                    <div className='height'>--</div>
+                </div>
+
+                <p>10 centimeters</p>
+                <div className='cm-measurement'>
+                    <div className='cm-length'>|</div>
+                    <div className='cm-height'>--</div>
+                </div>
+
                 <p>measure instructions</p>
             </div>
         )
