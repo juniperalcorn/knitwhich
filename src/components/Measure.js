e69ff347e3c1a1c50832e44edacabf1950743385
Juniper Alcorn <jalcorn@ool-ad02028b.dyn.optonline.net>
2019-03-26 11:38:56 -0400

    Measurement and gauge instructions

--- src/components/Measure.js
+++ src/components/Measure.js
@@ -9,11 +9,11 @@ class Measure extends Component{
             <div className='measure'>
                 <h1>Measure</h1>
                 
-                <p>2 inches</p>
+                {/* <p>2 inches</p>
                 <div className='inch-measurement'>
                     <div className='length'>|</div>
                     <div className='height'>--</div>
-                </div>
+                </div> */}
 
                 <p>10 centimeters</p>
                 <div className='cm-measurement'>
@@ -21,7 +21,11 @@ class Measure extends Component{
                     <div className='cm-height'>--</div>
                 </div>
 
-                <p>measure instructions</p>
+                <h4>Measurement Instructions</h4>
+                <p>Use this 10cm measurement to accurately check your knitting progress!</p>
+                <p>1 inch = 2.54 cm</p>
+                <p>It is not reccomended to use this to measure gauge, because your knitting will cover the screen. However, you can trace this square onto note paper and make your own ruler, and even add inches to the ruler by building blocks next to each other.</p>
+                <p>For thorough gauge instructions, check out this <a href='https://www.wikihow.com/Measure-Knit-Gauge'>Wiki How Page.</a></p>
             </div>
         )
     }
