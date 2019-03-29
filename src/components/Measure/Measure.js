3a36a2096b1677b9834d6e5063a91c80599c9421
Juniper Alcorn <jalcorn@ool-ad02028b.dyn.optonline.net>
2019-03-29 11:00:05 -0400

    Finish iphone and adjust measurements

--- src/components/Measure/Measure.js
+++ src/components/Measure/Measure.js
@@ -7,14 +7,14 @@ class Measure extends Component{
             <div className='measure'>
                 <h2>Measure</h2>
 
-                <p>10 centimeters</p>
+                <p>2 inches</p>
                 <div className='cm-measurement'>
                     <div className='cm-length'>|</div>
                     <div className='cm-height'>--</div>
                 </div>
 
                 <h4>Measurement Instructions</h4>
-                <p>Use this 10cm measurement to accurately check your knitting progress!</p>
+                <p>Use this 2in measurement to accurately check your knitting progress!</p>
                 <p>1 inch = 2.54 cm</p>
                 <p>It is not reccomended to use this to measure gauge, because your knitting will cover the screen. However, you can trace this square onto note paper and make your own ruler, and even add inches to the ruler by building blocks next to each other.</p>
                 <p>For thorough gauge instructions, check out this<a href='https://www.wikihow.com/Measure-Knit-Gauge'>Wiki How Page.</a></p>
