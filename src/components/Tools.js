26096116fe7846b08096028b612fd1dc866834dc
Juniper Alcorn <jalcorn@ool-ad02028b.dyn.optonline.net>
2019-03-29 11:09:21 -0400

    Remove measure component

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -5,7 +5,7 @@ function Tools () {
         return(
             <div className='Tools'>
                 <h2>Tools</h2>
-                <Link to='/measure'>Measure</Link>
+                {/* <Link to='/measure'>Measure</Link> */}
                 <Link to='/needles'>My Needles</Link>
                 <Link to='/rowCount'>My Row Counters</Link>
             </div>
